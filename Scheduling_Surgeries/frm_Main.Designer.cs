4a8e388a2cb3825fd50d3388dcd126e5f7c96576
ali zeinali <ali.zeinali70@gmail.com>
2022-08-16 20:46:50 +0430

    Add project files.

---
+++ Scheduling_Surgeries/frm_Main.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Scheduling_Surgeries
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Doctor = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Nurse = new System.Windows.Forms.Button();
+            this.btn_Surgery = new System.Windows.Forms.Button();
+            this.btn_Surgery_Room = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.btn_Do_Surgery = new System.Windows.Forms.Button();
+            this.btn_Report = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Doctor
+            // 
+            this.btn_Doctor.Location = new System.Drawing.Point(12, 12);
+            this.btn_Doctor.Name = "btn_Doctor";
+            this.btn_Doctor.Size = new System.Drawing.Size(79, 67);
+            this.btn_Doctor.TabIndex = 0;
+            this.btn_Doctor.Text = "Doctor";
+            this.btn_Doctor.UseVisualStyleBackColor = true;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(748, 551);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(79, 67);
+            this.btn_Exit.TabIndex = 1;
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            // 
+            // btn_Nurse
+            // 
+            this.btn_Nurse.Location = new System.Drawing.Point(97, 12);
+            this.btn_Nurse.Name = "btn_Nurse";
+            this.btn_Nurse.Size = new System.Drawing.Size(79, 67);
+            this.btn_Nurse.TabIndex = 2;
+            this.btn_Nurse.Text = "Nurse";
+            this.btn_Nurse.UseVisualStyleBackColor = true;
+            // 
+            // btn_Surgery
+            // 
+            this.btn_Surgery.Location = new System.Drawing.Point(182, 12);
+            this.btn_Surgery.Name = "btn_Surgery";
+            this.btn_Surgery.Size = new System.Drawing.Size(79, 67);
+            this.btn_Surgery.TabIndex = 3;
+            this.btn_Surgery.Text = "Surgery";
+            this.btn_Surgery.UseVisualStyleBackColor = true;
+            // 
+            // btn_Surgery_Room
+            // 
+            this.btn_Surgery_Room.Location = new System.Drawing.Point(267, 12);
+            this.btn_Surgery_Room.Name = "btn_Surgery_Room";
+            this.btn_Surgery_Room.Size = new System.Drawing.Size(91, 67);
+            this.btn_Surgery_Room.TabIndex = 4;
+            this.btn_Surgery_Room.Text = "Surgery_Room";
+            this.btn_Surgery_Room.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(364, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(79, 67);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Patient";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // btn_Do_Surgery
+            // 
+            this.btn_Do_Surgery.Location = new System.Drawing.Point(449, 12);
+            this.btn_Do_Surgery.Name = "btn_Do_Surgery";
+            this.btn_Do_Surgery.Size = new System.Drawing.Size(79, 67);
+            this.btn_Do_Surgery.TabIndex = 6;
+            this.btn_Do_Surgery.Text = "Do_Surgery";
+            this.btn_Do_Surgery.UseVisualStyleBackColor = true;
+            // 
+            // btn_Report
+            // 
+            this.btn_Report.Location = new System.Drawing.Point(534, 12);
+            this.btn_Report.Name = "btn_Report";
+            this.btn_Report.Size = new System.Drawing.Size(79, 67);
+            this.btn_Report.TabIndex = 7;
+            this.btn_Report.Text = "Report";
+            this.btn_Report.UseVisualStyleBackColor = true;
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(839, 630);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_Report);
+            this.Controls.Add(this.btn_Do_Surgery);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btn_Surgery_Room);
+            this.Controls.Add(this.btn_Surgery);
+            this.Controls.Add(this.btn_Nurse);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Doctor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "frm_Main";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Doctor;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_Nurse;
+        private System.Windows.Forms.Button btn_Surgery;
+        private System.Windows.Forms.Button btn_Surgery_Room;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btn_Do_Surgery;
+        private System.Windows.Forms.Button btn_Report;
+    }
+}
+
